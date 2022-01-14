4fadc8874d5dfc6ceb40d7497d839ae32dc3621b
Derek Reid <itsjustdel@gmail.com>
2022-01-14 14:04:00 +0000

    Deck Initialise and Shuffle

--- client/src/components/Game.js
+++ client/src/components/Game.js
@@ -3,36 +3,79 @@ import Dealer from "./Dealer"
 import Player from "./Player"
 
 const Game=() => {
+    
 
     const [currentPlayer,setCurrentPlayer]=useState(0)
     const [playerHand,setPlayerHand]=useState([])
     const [dealerHand,setDealerHand]=useState([])
     const [playerMoney,setPlayerMoney]=useState(100)
     const [playerBet,setPlayerBet]=useState(0)
-    
-    let deck=["1H","2H","3H","4H","5H","6H","7H","8H","9H","10H","JH","QH","KH",
-            "1C","2C","3C","4C","5C","6C","7C","8C","9C","10C","JC","QC","KC",
-            "1D","2D","3D","4D","5D","6D","7D","8D","9D","10D","JD","QD","KD",
-            "1S","2S","3S","4S","5S","6S","7S","8S","9S","10S","JS","QS","KS"]
-            //does deck need to be a useState?? not rendered anywhere
+    const [deck, setDeck] = useState( initialiseDeck() )
 
-            // is deck stored in database?
+    //refreshes render when state changes
+    useEffect(()=>{
+        //only on first load...
+        shuffleDeck();
+      },[]);
     
-    function shuffleDeck(deck) {
-        deck = deck.sort(() => Math.random() - 0.5)
+    
+            
+    function initialiseDeck()
+    {
+        let deck=["1H","2H","3H","4H","5H","6H","7H","8H","9H","10H","JH","QH","KH",
+        "1C","2C","3C","4C","5C","6C","7C","8C","9C","10C","JC","QC","KC",
+        "1D","2D","3D","4D","5D","6D","7D","8D","9D","10D","JD","QD","KD",
+        "1S","2S","3S","4S","5S","6S","7S","8S","9S","10S","JS","QS","KS"]
+    
+        return deck;
+
+    }
+
+    function shuffleDeck() {
+        console.log("shuffle Deck fired");
+        console.log(deck);
+        //make copy of array
+        let tempDeck = deck.map(s =>s);        
+        //random sort (this works)
+        tempDeck.sort(() => Math.random() - 0.5)
+        console.log(tempDeck);
+        //
+        setDeck(tempDeck);
         console.log(deck);
     }
     
+    function dealCards() {
+        console.log(deck);
+        //shift takes from array and saves in variable
+        //Player
+        let twoCards = [];             
+        twoCards.push( deck.shift() );
+        twoCards.push( deck.shift() );
+        setPlayerHand(twoCards);
 
-    const shuffledDeck = shuffleDeck(deck);
-    console.log(shuffledDeck);
+        //Dealer
+        twoCards = [];        
+        twoCards.push( deck.shift() );
+        twoCards.push( deck.shift() );
+        setDealerHand(twoCards);
+
+        console.log(deck);
+
+    }
+
+    //const shuffledDeck = shuffleDeck(deck);
+    //const dealtCards = dealCards();
+    //console.log(shuffledDeck);
+
+    
 
     return(
         <>
         <h4>Game lol</h4>
         <Dealer />
         <Player />
-        {shuffledDeck}
+        
+        
         </>
     )
 }
--- client/src/containers/GameContainer.js
+++ client/src/containers/GameContainer.js
@@ -1,9 +1,13 @@
-import React from "react"
+import React, { useEffect } from "react"
 import Game from "../components/Game"
 import PlayerList from "../components/PlayerList"
 import PlayerForm from "../components/PlayerForm"
 
 const GameContainer=() => {
+
+    useEffect( () => {
+      }, []);
+      
     return(
         <>
             <h2>something</h2>
