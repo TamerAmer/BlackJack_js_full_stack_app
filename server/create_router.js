9ea9751b2d7d375ab1650fe1b3e6a0f2576a2f9e
Derek Reid <itsjustdel@gmail.com>
2022-01-14 18:47:31 +0000

    router index

---
+++ server/create_router.js
@@ -0,0 +1,21 @@
+const express = require('express');
+//used to read id from db
+const ObjectID = require('mongodb').ObjectID;
+
+const createRouter = function (collection) {
+    const router = express.Router();
+
+    //INDEX - GET ALL 
+    router.get('/', (req, res) => {
+      //takes our games/collection and presents it as an array async - promise
+      collection.find().toArray()
+        .then(docs => res.json(docs))
+        .catch((err) => {
+          console.error(err);
+          //set status to internal server error
+          res.status(500);
+          //send client info on error
+          res.json({ status: 500, error: err });
+        });
+    });
+}
