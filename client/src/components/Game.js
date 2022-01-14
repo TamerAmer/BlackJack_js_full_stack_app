494bc9ee4f9e6d05e63ab5623fc352f2e7802056
Tamer <Tamer_amer@outlook.com>
2022-01-14 16:14:50 +0000

    fixed merger bugs

--- client/src/components/Game.js
+++ client/src/components/Game.js
@@ -26,10 +26,12 @@ const Game=() => {
         "1C","2C","3C","4C","5C","6C","7C","8C","9C","10C","JC","QC","KC",
         "1D","2D","3D","4D","5D","6D","7D","8D","9D","10D","JD","QD","KD",
         "1S","2S","3S","4S","5S","6S","7S","8S","9S","10S","JS","QS","KS"]
-    
+
+        return deck
+    }
 
 
-    function shuffleDeck() {
+    const shuffleDeck=() =>{
         //make copy of array
         let shuffledDeck = deck.map(s =>s);        
         //random sort (this works)
@@ -69,7 +71,6 @@ const Game=() => {
         <h4>Game lol</h4>
         <Dealer />
         <Player onBetSubmit={onBetSubmit} onBetClear={onBetClear}/>
-        {shuffledDeck}
 
         </>
     )
