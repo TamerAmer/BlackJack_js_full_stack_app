764857173d7d2fcd0f957b8c2400529e54955473
Derek Reid <itsjustdel@gmail.com>
2022-01-14 19:08:25 +0000

    create route

--- server/helpers/create_router.js
+++ server/helpers/create_router.js
@@ -3,24 +3,24 @@ const express = require('express');
 const ObjectID = require('mongodb').ObjectID;
 
 const createRouter = function (collection) {
-    const router = express.Router();
+  const router = express.Router();
 
-    //INDEX - GET ALL 
-    router.get('/', (req, res) => {
-      //takes our games/collection and presents it as an array async - promise
-      collection.find().toArray()
-        .then(docs => res.json(docs))
-        .catch((err) => {
-          console.error(err);
-          //set status to internal server error
-          res.status(500);
-          //send client info on error
-          res.json({ status: 500, error: err });
-        });
-    });
+  //INDEX - GET ALL 
+  router.get('/', (req, res) => {
+    //takes our games/collection and presents it as an array async - promise
+    collection.find().toArray()
+      .then(docs => res.json(docs))
+      .catch((err) => {
+        console.error(err);
+        //set status to internal server error
+        res.status(500);
+        //send client info on error
+        res.json({ status: 500, error: err });
+      });
+  });
 
-    //SHOW - GET ON BY ID
-    router.get('/:id', (req, res) => {
+  //SHOW - GET ON BY ID
+  router.get('/:id', (req, res) => {
     const id = req.params.id;
     //use ObjectID to convert id 
     collection
@@ -33,7 +33,20 @@ const createRouter = function (collection) {
       });
   });
 
-    return router;
+  //CREATE
+  router.post('/', (req, res) => {
+    const newData = req.body;
+    //insertOne returns a"result"(banana) object which we can then use to show
+    collection.insertOne(newData)
+      .then((result) => res.json(result.ops[0]))
+      .catch((err) => {
+        console.error(err);
+        res.status(500);
+        res.json({ status: 500, error: err });
+      });
+  });
+
+  return router;
 }
 
 module.exports = createRouter;
